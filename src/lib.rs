f5f55fd69226ad5efb44b87f788d8cdf4ed85983
Paolo Raez <str237@gmail.com>
2022-03-18 23:04:40 +0100

    Use supportedExtension

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
+use serde::Deserialize;
+use std::convert::{TryFrom, TryInto};
 use std::fs;
 use std::process::Command;
-use serde::{Deserialize};
 use toml;
-use std::convert::TryFrom;
 
 pub mod error;
 
@@ -15,38 +15,38 @@ pub fn run() -> DynResult<()> {
 
 #[derive(Deserialize, Debug)]
 struct FileField {
-  version: String
+  version: String,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum SupportedExtensions {
   Toml,
-  Json
+  Json,
 }
 
 impl TryFrom<&str> for SupportedExtensions {
-    type Error = ();
+  type Error = AppError;
 
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
-      let res: Self = match value {
-        "toml" => SupportedExtensions::Toml,
-        "json"=> SupportedExtensions::Json,
-        _ => return Err(())
-      };
-      Ok(res)
-    }
+  fn try_from(value: &str) -> Result<Self, Self::Error> {
+    let res: Self = match value {
+      "toml" => SupportedExtensions::Toml,
+      "json" => SupportedExtensions::Json,
+      _ => return Err(AppError::UnhandleExtension),
+    };
+    Ok(res)
+  }
 }
 
 pub fn read_vesion_from_file(filename: &str) -> DynResult<String> {
-  let extension = filename .split(".").last();
-  if extension.is_none() {
-    return Err(Box::new(AppError::ExtensionNotFound))
-  }
-  let extension = extension.unwrap();
+  let extension: SupportedExtensions = filename
+    .split(".")
+    .last()
+    .ok_or_else(|| AppError::ExtensionNotFound)?
+    .try_into()?;
   let file = fs::read_to_string(filename)?;
   let parsed: DynResult<FileField> = match extension {
-    "toml" => toml::from_str(&file).map_err(|err| err.into()),
-    _ => Err(AppError::UnhandleExtension.into())
+    SupportedExtensions::Toml => toml::from_str(&file).map_err(|err| err.into()),
+    SupportedExtensions::Json => todo!(),
   };
   let parsed: FileField = parsed?;
   Ok("".to_string())
